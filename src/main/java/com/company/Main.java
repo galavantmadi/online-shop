bf9b7c33d6fa0cce7af27500961c9e808a166647
galavantmadi <galavantmadi@icloud.com>
2023-03-30 16:30:17 +0430

    menu grid product and show item with list view dynamic

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -19,8 +19,30 @@ public class Main extends Application {
         admin=new Admin(1,"peyman","123","09391120290", AccountType.ADMIN,true,"","peyman@.com");
         ArrayList<Admin> admins=new ArrayList<>();
         admins.add(admin);
+        Category category=new Category(1,"کالای دیجیتال");
+        Category category2=new Category(2,"موبایل");
+        Category category3=new Category(3,"مد و پوشاک");
+        Category category4=new Category(4,"اسباب بازی");
+        Category category5=new Category(5,"زیبایی");
+        Category category6=new Category(6,"ورزش و سلامت");
+        Category category7=new Category(7,"کتاب");
+        Category category8=new Category(8,"سفر");
+        Category category9=new Category(9,"ابزار صنعتی");
+        Category category10=new Category(10,"سوپر مارکت");
+        ArrayList<Category> categories=new ArrayList<>();
+        categories.add(category);
+        categories.add(category2);
+        categories.add(category3);
+        categories.add(category4);
+        categories.add(category5);
+        categories.add(category6);
+        categories.add(category7);
+        categories.add(category8);
+        categories.add(category9);
+        categories.add(category10);
+
         shopService=new ShopService(new ArrayList<>(),new ArrayList<>(),admins,new ArrayList<>()
-                ,new ArrayList<>(),new ArrayList<>(),new ArrayList<>(),new User(),admin,new Seller());
+                ,new ArrayList<>(),categories,new ArrayList<>(),new User(),admin,new Seller());
 
         launch(args);
 
--- src/main/java/com/company/controller/MainPageController.java
+++ src/main/java/com/company/controller/MainPageController.java
@@ -1,20 +1,30 @@
 package com.company.controller;
 
 import com.company.Main;
+import com.company.model.Admin;
+import com.company.model.Product;
+import com.company.model.User;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 
 public class MainPageController implements Initializable {
@@ -31,10 +41,26 @@ public class MainPageController implements Initializable {
     @FXML
     private Label lblUser;
 
+    @FXML
+    private Label welcomeLBL;
+
+    @FXML
+    private ToolBar toolbarId;
+
+    @FXML
+    private ListView productLSV;
+
+    @FXML
+    private Label categoryLBL;
+    @FXML private Pane topPanelId;
+
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-init();
+
+        init();
 
         enterLNK.setOnAction(c->{
             root.getScene().getWindow().hide();
@@ -73,17 +99,80 @@ init();
     }
 
     public void init(){
+        categoryLBL.layoutXProperty().bind(topPanelId.widthProperty().subtract(categoryLBL.widthProperty()).divide(1));
         if(Main.shopService.getUser().getId()==0){
             lblUser.setText("");
             lblUser.setVisible(false);
+            welcomeLBL.setVisible(false);
             enterLNK.setVisible(true);
             registerLNK.setVisible(true);
         }else {
-            lblUser.setText("  خوش آمدید "+Main.shopService.getUser().getUsername());
+            lblUser.setText(Main.shopService.getUser().getUsername());
             lblUser.setVisible(true);
+            welcomeLBL.setVisible(true);
             lblUser.setTextFill(Color.BLUE);
+            welcomeLBL.setTextFill(Color.BLUE);
             enterLNK.setVisible(false);
             registerLNK.setVisible(false);
+        }
+        Main.shopService.getCategoryList().forEach(c->{
+            Hyperlink hyperlink=new Hyperlink(c.getTitle());
+            hyperlink.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    Hyperlink source=(Hyperlink) event.getSource();
+                    loadMAinProduct(source.getText());
+                    System.out.println(source.getText());
+                }
+            });
+            toolbarId.getItems().add(hyperlink);
+        });
+
+
+        loadMAinProduct("");
+
+        productLSV.setCellFactory(param -> new ProductListCell());
+    }
+
+    public void loadMAinProduct(String name){
+        productLSV.getItems().clear();
+        ArrayList<Product> products;
+        if(name.equals("")){
+            products=Main.shopService.getProductList();
+        }else {
+            products=Main.shopService.getProductList().stream()
+                    .filter(c->c.getCategory().getTitle().equals(name)).collect(Collectors.toCollection(ArrayList::new));
+        }
+
+        // Copies data into the TableView's items list
+        productLSV.getItems().addAll(products);
+    }
+
+    private static class ProductListCell extends ListCell<Product>{
+        private final Label title = new Label();
+        private final Label detail = new Label();
+        private final VBox layout = new VBox(title, detail);
+
+        public ProductListCell() {
+            super();
+            title.setStyle("-fx-font-size: 20px;");
+        }
+
+        @Override
+        protected void updateItem(Product item, boolean empty) {
+            super.updateItem(item, empty);
+            setText(null);
+
+            if (empty || item == null || item.getId() == 0) {
+                title.setText(null);
+                detail.setText(null);
+                setGraphic(null);
+            } else {
+                title.setText(item.getName());
+                detail.setText(item.getPrice()+" "+item.getSeller().getCompanyName());
+
+                setGraphic(layout);
+            }
         }
     }
 }
